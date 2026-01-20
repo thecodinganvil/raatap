4c8be46d2d8d234210fa2c8670d24a878c82b60a
Mohammed Osman <muhammedosmanraheemkhan@gmail.com>
2026-01-20 22:47:45 +0530

    lint error for commas

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -106,7 +106,7 @@ export default function Login() {
           
           {/* Sign up link */}
           <p className="text-center text-sm text-gray-600 mt-6">
-            Don't have an account?{" "}
+            Don&apos;t have an account?{" "}
             <Link href="/signup" className="text-[#6675FF] font-medium hover:underline">
               Sign up
             </Link>
