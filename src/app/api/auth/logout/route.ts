75a96e28dcf0e0a73d39d1c660ef828db64702d1
Mohammed Osman <muhammedosmanraheemkhan@gmail.com>
2026-01-31 08:20:23 +0530

    post issue fixed

--- src/app/api/auth/logout/route.ts
+++ src/app/api/auth/logout/route.ts
@@ -13,7 +13,13 @@ export async function POST() {
         getAll() {
           return cookieStore.getAll();
         },
-        setAll(cookiesToSet) {
+        setAll(
+          cookiesToSet: {
+            name: string;
+            value: string;
+            options: CookieOptions;
+          }[],
+        ) {
           try {
             cookiesToSet.forEach(({ name, value, options }) =>
               cookieStore.set(name, value, options),
